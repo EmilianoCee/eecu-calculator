cbe44baf48419f81fc1d9ba37e29c8e05a0faf84
Cereceres Emiliano <emiliano.cereceres@student.cart.org>
2023-01-30 10:44:01 -0800

    js sorting
    sorting by name and income created in js

--- javascript/script.js
+++ javascript/script.js
@@ -4,6 +4,9 @@ const jobList = document.getElementById("job-list");
 const sortButtons = document.getElementsByClassName("job-sort")
 const sortTriangles = document.getElementsByClassName("triangle");
 
+const nameSort = document.getElementById("name-sort");
+const incomeSort = document.getElementById("income-sort");
+
 const displayedJobs = document.getElementsByClassName("job");
 const jobTitle = document.getElementById("info-job");
 const grossAnnual = document.getElementById("gross-annual");
@@ -27,69 +30,74 @@ const searchInput = document.getElementById("search");
 
 const jobs = [
     {work: "Accountant", income: 55650}, 
-    {work: "Advance Tractor/Trailer Driver", income: 53550},
-    {work: "Zebra Breeder", income: 929292},
+    {work: "Advance Tractor / Trailer Driver", income: 53550},
     {work: "Agricultural Engineer", income: 56700},
-    {work: "Architech", income: 53550},
-    {work: "Auto Tech/Mechanic", income: 49350},
+    {work: "Architect", income: 53550},
+    {work: "Auto Tech / Mechanic", income: 49350},
     {work: "Aviation Tech. Mechanic", income: 50400},
     {work: "Biologist", income: 54600},
     {work: "Bus Driver", income: 37800},
-    {work: "homeless", income: 24}, 
     {work: "Business Development Officer", income: 54600},
     {work: "Business Manager (Hotel, etc.)", income: 61950},
-    {work: "homeless", income: 24}, 
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24}, 
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24}, 
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24},
-    {work: "homeless", income: 24}
+    {work: "CNC Manufacturing", income: 80850}, 
+    {work: "Carpenter", income: 47250},
+    {work: "Chef", income: 52500},
+    {work: "Civil Engineering Technician", income:68250},
+    {work: "Commercial Driver ", income: 51450},
+    {work: "Computer Technician", income: 46200},
+    {work: "Conserv. / Environ. Science", income: 72450},
+    {work: "Corectional Officer", income: 48300},
+    {work: "Cosmetologist", income: 36750},
+    {work: "Credit Union / Bank Manager", income:61950},
+    {work: "Daycare Director", income: 37800},
+    {work: "Dentist", income: 115500},
+    {work: "Detective", income: 60900},
+    {work: "Diesel Tech / Mechanic ", income:55650},
+    {work: "Doctor / Physician", income: 147000},
+    {work: "Electrician", income: 57600},
+    {work: "Electronic Engineer", income: 75600},
+    {work: "EMT", income: 34650}, 
+    {work: "Energy Management PG&E", income: 106050},
+    {work: "Engineer", income: 72450},
+    {work: "Fashion Designer", income: 63000},
+    {work: "Fire Fighter", income: 49350},
+    {work: "Forest Ranger ", income: 52500},
+    {work: "Graphic / Media Designer ", income: 58800},
+    {work: "H/C HVAC", income: 63000},
+    {work: "Highway Patrol", income: 84000},
+    {work: "Home Inspector", income: 56700},
+    {work: "Industrial Mechanic", income: 56700},
+    {work: "Interior Designer", income: 46200},
+    {work: "Investment Analyst", income: 49350},
+    {work: "Lab Technician", income: 66150},
+    {work: "Landscaper Horticulture", income: 48300},
+    {work: "Lawyer", income: 86100},
+    {work: "Marketing / Sales Manager", income: 48800},
+    {work: "Media / Communications", income: 47150},
+    {work: "Medical Repair Tech.", income: 52500}, 
+    {work: "Military", income: 55650},
+    {work: "Nurse", income: 66150},
+    {work: "Nutritionist", income: 45150},
+    {work: "Oceanographer", income: 69300},
+    {work: "Pastor", income: 50400},
+    {work: "PG&E / AT8&T Technician", income:78750},
+    {work: "Pharmacist", income: 105000},
+    {work: "Photographer", income: 45150},
+    {work: "Physical Therapist", income: 72450},
+    {work: "Pilot (Commercial) ", income: 78750},
+    {work: "Plumber", income: 52500},
+    {work: "Police Officer", income: 53550},
+    {work: "Principal", income: 93450},
+    {work: "Probation Officer", income: 44100},
+    {work: "Psychologist", income: 77700},
+    {work: "Retail Sales Associate", income:34650},
+    {work: "Social Worker ", income: 50400},
+    {work: "Solar Energy Tech.", income:53550},
+    {work: "Teacher", income: 52500},
+    {work: "UPS / Fed Ex Driver", income: 68250},
+    {work: "Veterinarian", income: 82950},
+    {work: "Welder / Metal Specialist", income: 47250},
+    {work: "Wind Energy Technician", income: 56700},
 ]
 
 populateTemplates();
@@ -130,13 +138,21 @@ for (let i = 0; i < sortButtons.length; i++) {
         const innerTriangle = this.children[1].classList;
         if (innerTriangle.contains("triangle-left") || innerTriangle.contains("triangle-down")) {
             resetTriangles();
-            sortIncomeU()
+            if (this.id == incomeSort.id) {
+                sortIncomeU();
+            } else {
+                sortNameDown();
+            }
             innerTriangle.remove("triangle-right");
             innerTriangle.add("triangle-up");
 
         } else if (innerTriangle.contains("triangle-up")) {
             resetTriangles();
-            sortIncome()
+            if (this.id == incomeSort.id) {
+                sortIncome();
+            } else {
+                sortNameUp();
+            }
             innerTriangle.remove("triangle-right");
             innerTriangle.add("triangle-down");
         } else {
@@ -154,6 +170,17 @@ function resetTriangles() {
         element.add("triangle-right");
     }
 }
+
+let toggle = true;
+nameSort.addEventListener("click", function() {
+    if (!toggle) {
+        sortNameUp();
+    } else {
+        sortNameDown();
+    }
+    toggle = !toggle;
+})
+
 
 function clickableJobs() {
 for (let i = 0; i < displayedJobs.length; i++) {
@@ -191,15 +218,6 @@ for (let i = 0; i < displayedJobs.length; i++) {
     })
 }};
 
-function sortJobs() {
-    jobs.sort(function(a, b) {
-        let nameA = a.work.toLowerCase();
-        let nameB = b.work.toLowerCase();
-        
-        // jobs.sort((nameA, nameB) => nameA.localeCompare(nameB));
-    })
-}
-
 function sortIncome() {
     jobs.sort((a, b) => a.income - b.income)
     removeTemplates()
@@ -222,8 +240,45 @@ function removeTemplates() {
 
 const testButton = document.getElementById("test");
 testButton.addEventListener("click", function() {
-    console.log(jobs.length)
+    console.log(sortButtons[1].id == incomeSort.id);
+    sortNameDown();
 })
+
+function sortNameDown() {
+    jobs.sort(function(a, b) {
+        let nameA = a.work.toLowerCase();
+        let nameB = b.work.toLowerCase();
+        if (nameA < nameB) {
+            return 0;
+        } 
+        if (nameA > nameB) {
+            return -1
+        }
+        return 1;
+    })
+    jobs.forEach((e) => {
+        removeTemplates()
+        populateTemplates()
+    })
+}
+
+function sortNameUp() {
+    jobs.sort(function(a, b) {
+        let nameA = a.work.toLowerCase();
+        let nameB = b.work.toLowerCase();
+        if (nameA < nameB) {
+            return -1;
+        } 
+        if (nameA > nameB) {
+            return 1
+        }
+        return 0;
+    })
+    jobs.forEach((e) => {
+        removeTemplates()
+        populateTemplates()
+    })
+}
 
 // jobs.sort(function(a, b) {
 //     let nameA = a.work.toLowerCase();
